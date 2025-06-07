653034333bbde0d892929d04b7917ca0c4466f0a
piyushj226 <piyushjainpc098@gmail.com>
2025-06-07 16:10:37 +0530

    Delete app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-
-const express = require('express');
-const app = express();
-app.get('/', (req, res) => {
-  res.send('Hello from K8s!');
-});
-app.listen(3000, () => {
-  console.log('App running on port 3000');
-});
